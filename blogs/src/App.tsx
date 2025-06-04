13124144104cb0b8e64f66a3979daac5538bd4cc
dk-willing <davidkarikari89@gmail.com>
2025-06-04 23:21:07 +0000

    Create modal for new blog

--- blogs/src/App.tsx
+++ blogs/src/App.tsx
@@ -1,23 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import Navigation from "./components/Navigation";
 import PeopleToFollow from "./recomendation/PeopleToFollow";
 import TrendList from "./posts/TrendList";
 import TopicList from "./posts/TopicList";
+import { BlogProvider } from "./appContext/BlogContext";
+import { IoAddCircle } from "react-icons/io5";
+import { Blog } from "./types/BlogType";
 
 const App = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [editingBlog, setEditingBlog] = useState<Blog | null>(null);
+
+  // Temporal Variable
+
+  const blog = {
+    id: 2,
+    title: "This must be deleted",
+    description: "This would be deleted",
+    image: "https://something.com",
+    time: "3:00am",
+  };
+
+  function openModalForNewBlog() {
+    setIsModalOpen(true);
+    setEditingBlog(null);
+  }
+
+  function openModalForEdit() {
+    setIsModalOpen(false);
+    setEditingBlog(blog);
+  }
+
   return (
     <div>
-      <Navigation />
+      <BlogProvider>
+        <Navigation />
 
-      <div className="flex justify-center">
-        {/* Main Area */}
+        <div className="flex justify-center">
+          {/* Main Area */}
 
-        <div className="w-[30%]">
-          <PeopleToFollow />
-          <TrendList />
-          <TopicList />
+          <section className="mx-auto p-6">
+            <div>
+              <button
+                onClick={openModalForNewBlog}
+                className="flex justify-center items-center bg-black text-white rounded px-4 py-2 "
+              >
+                Add New Blog <IoAddCircle className="ml-[.5rem]" />
+              </button>
+
+              {/* Article List */}
+
+              {/* {isModalOpen && <Modal></Modal>} */}
+            </div>
+          </section>
+
+          <div className="w-[30%]">
+            <PeopleToFollow />
+            <TrendList />
+            <TopicList />
+          </div>
         </div>
-      </div>
+      </BlogProvider>
     </div>
   );
 };
--- blogs/src/appContext/BlogContext.tsx
+++ blogs/src/appContext/BlogContext.tsx
@@ -1,3 +1,45 @@
-import { createContext, useState, useContext } from "react";
+import React, { createContext, useState, useContext } from "react";
+import type { Blog } from "../types/BlogType";
 
-const BlogContextProps = createContext(undefined);
+interface BlogType {
+  blogs: Blog[];
+  addBlog: (blog: Blog) => void;
+  updateBlog: (blog: Blog) => void;
+  deleteBlog: (id: number) => void;
+}
+
+const BlogContextProps = createContext<BlogType | undefined>(undefined);
+
+export const BlogProvider = ({ children }: { children: React.ReactNode }) => {
+  const [blogs, setBlogs] = useState<Blog[]>([]);
+
+  function addBlog(blog: Blog) {
+    setBlogs([...blogs, blog]);
+  }
+
+  function updateBlog(update: Blog) {
+    setBlogs(blogs.map((blog) => (blog.id === update.id ? update : blog)));
+  }
+
+  function deleteBlog(id: number) {
+    setBlogs(blogs.filter((blog) => blog.id !== id));
+  }
+
+  return (
+    <BlogContextProps.Provider
+      value={{ blogs, addBlog, deleteBlog, updateBlog }}
+    >
+      {children}
+    </BlogContextProps.Provider>
+  );
+};
+
+export const useBlogs = () => {
+  const context = useContext(BlogContextProps);
+
+  if (!context) {
+    throw new Error("Context must be used within a BlogProvider");
+  }
+
+  return context;
+};
