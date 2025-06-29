cb13029bf58cce0527d213172b9ce7fafce10111
dk-willing <davidkarikari89@gmail.com>
2025-06-29 19:53:18 +0000

    Added React Essentials Project

---
+++ first-project/src/App.jsx
@@ -0,0 +1,74 @@
+import { CORE_CONCEPTS } from "./data";
+import { EXAMPLES } from "./data";
+import Header from "./components/Header/Header";
+import CoreConcepts from "./components/CoreConcepts";
+import TabButton from "./components/TabButton";
+import { useState } from "react";
+
+function App() {
+  const [selectedTopic, setSelectedTopic] = useState("components");
+
+  function handleClick(selectedBtn) {
+    setSelectedTopic(selectedBtn);
+  }
+
+  let tabContent = <p>Please select a topic</p>;
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <ul>
+            {CORE_CONCEPTS.map((el) => (
+              <CoreConcepts key={Math.random()} {...el} />
+            ))}
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Example</h2>
+          <menu>
+            <TabButton
+              isSelected={selectedTopic === "components"}
+              onSelect={() => handleClick("components")}
+            >
+              Component
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "jsx"}
+              onSelect={() => handleClick("jsx")}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "props"}
+              onSelect={() => handleClick("props")}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "state"}
+              onSelect={() => handleClick("state")}
+            >
+              State
+            </TabButton>
+          </menu>
+          {tabContent}
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
